0a7076461c91364707662a568eb5f7863d19f7ae
imbenclifford <imbenclifford@gmail.com>
2014-09-22 15:37:48 +0000

    making title appear as link

--- js/application.js
+++ js/application.js
@@ -1,5 +1,21 @@
 $( document ).ready(function() {
 
-    window.alert("Hello, Camden Town");
-
+  $.ajax({
+    url: "http://content.guardianapis.com/search?api-key=test",
+    success: function(data) {
+      window.alert("Working!");
+      var arr = [
+        [data.response.results[0].webTitle,  data.response.results[0].webUrl],
+        [data.response.results[1].webTitle,  data.response.results[1].webUrl],
+        [data.response.results[2].webTitle,  data.response.results[2].webUrl],
+        [data.response.results[3].webTitle,  data.response.results[3].webUrl],
+        [data.response.results[4].webTitle,  data.response.results[4].webUrl]
+      ];
+      var str = '<a href="'+ arr[0][1] + '">' + arr[0][0] + "</a>";
+      window.alert(str)
+      $( "#weather2" ).html(str);
+    }
+});
+  
+     
 });
