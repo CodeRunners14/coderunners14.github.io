6955ffc36d537cc08bc7ab4243146527032f1844
imbenclifford <imbenclifford@gmail.com>
2014-09-23 10:50:17 +0000

    incldue three sections and make table

--- js/application.js
+++ js/application.js
@@ -1,13 +1,37 @@
 $( document ).ready(function() {
-
-  $.ajax({
-    url: "http://content.guardianapis.com/search?api-key=test",
+         
+$.ajax({
+   
+    url: "http://content.guardianapis.com/football?api-key=test",
     success: function(data) {
       $.each(data.response.results, function(i, result) {
         var str = '<a href="'+ this["webUrl"] + '">' + this["webTitle"] + "</a>";
-        $( "#weather2" ).append(str);
+        $( "#sport" ).append(str);
       });
-    }
-  }
-);  
+    }  
+  })
+
+$.ajax({
+   
+    url: "http://content.guardianapis.com/travel?api-key=test",
+    success: function(data) {
+      $.each(data.response.results, function(i, result) {
+        var str = '<a href="'+ this["webUrl"] + '">' + this["webTitle"] + "</a>";
+        $( "#travel" ).append(str);
+      });
+    }  
+  })
+
+$.ajax({
+   
+    url: "http://content.guardianapis.com/uk-news?api-key=test",
+    success: function(data) {
+      $.each(data.response.results, function(i, result) {
+        var str = '<a href="'+ this["webUrl"] + '">' + this["webTitle"] + "</a>";
+        $( "#uk-news" ).append(str);
+      });
+    }  
+  })
+       
+  
 });
