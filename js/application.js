83efddcb25caf57a2ec172b3c336d6c8b919e887
imbenclifford <imbenclifford@gmail.com>
2014-09-26 12:40:21 +0000

    append update 2

--- js/application.js
+++ js/application.js
@@ -1,23 +1,19 @@
 $( document ).ready(function() {
-
 var fillNews=function(sect){
-  
 $.ajax({
-   
     url: "http://content.guardianapis.com/search?api-key=test&show-fields=all&order-by=newest&section=" + sect,
     success: function(data) {
       
-      for(var i=0; i<5; i++) {
-        
-   var str = '<li class="row gtableline"><a class="col-md-8" href="'+
-            data.response.results[i].webUrl + '">'+'<img src="'+
-            data.response.results[i].fields.thumbnail + '" class="col-md-4" />' +
-            data.response.results[i].webTitle + "</a>" +
-            data.response.results[i].fields.trailText.substr(0,130)+"..." + '</li>';
-        
-        $( "#" + sect ).append(str);
+    var str = '';
+        for(var i=0; i<5; i++) {
+           str += '<li class="row gtableline"><a class="col-md-8" href="'
+               + data.response.results[i].webUrl + '">' + '<img src="'
+               + data.response.results[i].fields.thumbnail + '" class="col-md-4" />'
+               + data.response.results[i].webTitle + "</a>"
+               + data.response.results[i].fields.trailText.substr(0,130) + "..." + '</li>';
         }
-      }
+    $( "#" + sect ).append(str);
+    }
     });
   };
     fillNews("football");
