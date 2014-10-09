b5c45df0341583597c0e74022469d664e0aa9421
imbenclifford <imbenclifford@gmail.com>
2014-10-09 15:28:06 +0000

    appending images

--- js/AJAXtwit.js
+++ js/AJAXtwit.js
@@ -2,7 +2,14 @@ $( document ).ready(function(){
 $.ajax({
     url: "http://chrome-firefly-45-146848.euw1-2.nitrousbox.com:8080/",
     success: function(data) {
-       var str = data;
+       var array = JSON.parse(data);
+      
+      var str = '';
+        for(var i=0; i<5; i++) {
+           str += '<li class="row gtableline"><img src="'
+               + array[i] + '" class="col-md-4" /></li>';
+        }
+      console.log(str)
         $( "#ajaxy" ).append(str)
   }
 });
