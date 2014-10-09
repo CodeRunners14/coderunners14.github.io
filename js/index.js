135e472ecb100bcfafe801a0b8b76ba033a0e4d2
imbenclifford <imbenclifford@gmail.com>
2014-10-09 14:40:45 +0000

    ajax changes on right directory

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 var twitpic = require('./twitpic')
+var fs = require('fs')
+var http = require('http');
 
 twitpic();
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {
+    "Content-Type": "text/plain",
+    "Access-Control-Allow-Origin": "*"
+                          });
+  var arrayJSON = fs.readFileSync('array.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    return(data);
+  });
+  response.end(arrayJSON);
+  
+});
+
+server.listen(process.env.PORT || 8080);
